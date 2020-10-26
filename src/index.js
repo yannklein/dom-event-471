8c8d06599f36441bc66d54ebfd0726ec68e8a550
Yann Klein <yann.klein@me.com>
2020-10-26 21:49:30 +0900

    lecture baser

---
+++ src/index.js
@@ -0,0 +1,73 @@
+console.log("Hello from src/index.js!");
+
+
+// ////////
+// Select DOM elements
+// ////////
+
+// with its ID (seigo)
+
+
+// with its tag type (h2)
+
+
+// with its class (group)
+
+
+// by nest selection (ul of the group)
+
+
+// select multiple elements (all the card)
+
+
+// ////////
+// Modify the DOM
+// ////////
+
+// add an HTML element (Juventus di Escuela 🇮🇹 in list)
+
+
+// same but more complex (add Alex!!)
+
+
+// change the element style (group background to green)
+// Careful! background-color => backgroundColor
+
+
+// add a class to the element (add text-white to group)
+
+
+// remove it
+
+
+// add/remove every 1sec
+
+
+// Read/Write for inputs (change the value in the email input)
+
+
+// Read/Write the (inner) text and HTML content (Vip mentor club title)\
+
+
+
+// ////////
+// Add event listeners
+// ////////
+
+// //1. Select seigo's picture
+
+
+// 2. Listen to a click
+
+// 3. Modify the DOM (add the class selected)
+
+
+
+// 1. Select all pictures
+
+
+// 2. Listen to a click
+
+
+// 3. Modify the DOM
+
